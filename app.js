43cf00f4effa2c5e0d1cb91416d38aa47f6debfc
David <davidmcruz1125@gmail.com>
2020-08-31 17:42:41 -0500

    Testing and playing with react

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    console.log(this)
+    console.log(this.props)
+    return(
+      <li>{this.props.groceries.item} {this.props.groceries.quantity}</li>
+    )
+  }
+}
+
+var TodoList = (props) => {
+  console.log(props)
+  return (
+    <ul>
+      {props.groceries.map(obj =>
+        <TodoListItem groceries={obj} />)}
+    </ul>
+  )}
+
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList groceries={[{item: 'Apples', quantity: 3}, {item: 'Bananas', quantity: 3}, {item: 'Oranges', quantity: 3}]} />
+  </div>
+);
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
